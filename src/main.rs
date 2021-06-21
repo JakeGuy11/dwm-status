6d82c04813d9bf6abdcde61bb7cefa2ec06d06f4
Jake <Jake_Guy_11@protonmail.ch>
2021-06-21 17:42:32 -0400

    added CPU measurment

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate cpu_monitor;
+
+use std::thread;
+use std::time;
+use sysinfo::SystemExt;
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("Starting dwm-status... ");
+    
+    let mut current_sys = sysinfo::System::new();
+    current_sys.refresh_all();
+
+    let start = cpu_monitor::CpuInstant::now().unwrap();
+    thread::sleep(time::Duration::from_millis(1000));
+    let end = cpu_monitor::CpuInstant::now().unwrap();
+
+    let duration = end - start;
+    println! ("CPU: {:.1}%\tRAM:12.3%", duration.non_idle() * 100 as f64);
 }
