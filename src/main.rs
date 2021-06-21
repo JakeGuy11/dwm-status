574f5ea02424a3dbc59eaa613e3ae8d5fef8ff7e
Jake <Jake_Guy_11@protonmail.ch>
2021-06-21 18:07:13 -0400

    moved cpu info to function

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,19 @@ use std::time;
 use sysinfo::SystemExt;
 use sysinfo::ProcessorExt;
 
+fn get_cpu_avg(req_sys: sysinfo::System) -> f32
+{
+
+    let mut cpus: Vec<f32> = Vec::new();
+    for core in req_sys.get_processors() { cpus.push(core.get_cpu_usage()); }
+
+    let cpu_tot: f32 = cpus.iter().sum();
+    let cpu_avg: f32 = cpu_tot / cpus.len() as f32;
+
+    cpu_avg
+
+}
+
 fn main()
 {
     println!("Starting dwm-status... ");
@@ -13,11 +26,7 @@ fn main()
     let mut current_sys = sysinfo::System::new_all();
     current_sys.refresh_all();
 
-    let mut cpus: Vec<f32> = Vec::new();
-    for core in current_sys.get_processors() { cpus.push(core.get_cpu_usage()); }
-
-    let cpu_tot: f32 = cpus.iter().sum();
-    let cpu_avg: f32 = cpu_tot / cpus.len() as f32;
+    let cpu_avg = get_cpu_avg(current_sys);
 
     println! ("CPU: {:.1}", cpu_avg);
 }
