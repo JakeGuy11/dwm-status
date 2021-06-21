3d8341c2cfa7a6aa79184749e7a95864d3ce5c56
Jake <Jake_Guy_11@protonmail.ch>
2021-06-21 18:43:31 -0400

    added swap monitor

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ use sysinfo::ProcessorExt;
 
 fn get_cpu_use(req_sys: &sysinfo::System) -> f32
 {
-
     let mut cpus: Vec<f32> = Vec::new();
     for core in req_sys.get_processors() { cpus.push(core.get_cpu_usage()); }
 
@@ -16,12 +15,16 @@ fn get_cpu_use(req_sys: &sysinfo::System) -> f32
     let cpu_avg: f32 = cpu_tot / cpus.len() as f32;
 
     cpu_avg
-
 }
 
 fn get_ram_use(req_sys: &sysinfo::System) -> f32
 {
     (req_sys.get_used_memory() as f32) / (req_sys.get_total_memory() as f32) * 100.
+}
+
+fn get_swp_use(req_sys: &sysinfo::System) -> f32
+{
+    (req_sys.get_used_swap() as f32) / (req_sys.get_total_swap() as f32) * 100.
 }
 
 fn main()
@@ -33,6 +36,7 @@ fn main()
 
     let cpu_avg = get_cpu_use(&current_sys);
     let ram_prcnt = get_ram_use(&current_sys);
+    let swp_prcnt = get_swp_use(&current_sys);
 
-    println! ("CPU: {:.1}%\tRAM: {:.1}", cpu_avg, ram_prcnt);
+    println! ("CPU: {:.1}%\tRAM: {:.1}%\tSWP: {:.2}%", cpu_avg, ram_prcnt, swp_prcnt);
 }
