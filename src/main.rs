3db43752ae462591aa786b166c8a6695c1cd21a0
Jake <Jake_Guy_11@protonmail.ch>
2021-06-22 08:27:32 -0400

    added temperature monitoring

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::time;
 use sysinfo::SystemExt;
 use sysinfo::ProcessorExt;
 use sysinfo::NetworkExt;
-use sysinfo::NetworksExt;
+use sysinfo::ComponentExt;
 
 fn get_cpu_use(req_sys: &sysinfo::System) -> f32
 {
@@ -33,7 +33,7 @@ fn get_swp_use(req_sys: &sysinfo::System) -> f32
 fn get_ntwk_dwn(req_sys: &sysinfo::System) -> i32
 {
     let mut rcv_tot: Vec<i32> = Vec::new();
-    for (interface_name, ntwk) in req_sys.get_networks() { rcv_tot.push(ntwk.get_received() as i32); }
+    for (_interface_name, ntwk) in req_sys.get_networks() { rcv_tot.push(ntwk.get_received() as i32); }
 
     let ntwk_tot: i32 = rcv_tot.iter().sum();
     let ntwk_processed = (ntwk_tot / 128) as i32;
@@ -44,7 +44,7 @@ fn get_ntwk_dwn(req_sys: &sysinfo::System) -> i32
 fn get_ntwk_up(req_sys: &sysinfo::System) -> i32
 {
     let mut snd_tot: Vec<i32> = Vec::new();
-    for (interface_name, ntwk) in req_sys.get_networks() { snd_tot.push(ntwk.get_transmitted() as i32); }
+    for (_interface_name, ntwk) in req_sys.get_networks() { snd_tot.push(ntwk.get_transmitted() as i32); }
 
     let ntwk_tot: i32 = snd_tot.iter().sum();
     let ntwk_processed = (ntwk_tot / 128) as i32;
@@ -52,6 +52,13 @@ fn get_ntwk_up(req_sys: &sysinfo::System) -> i32
     ntwk_processed
 }
 
+fn get_temp(req_sys: &sysinfo::System) -> i32
+{
+    let mut wanted_temp: f32 = -1.;
+    for comp in req_sys.get_components() { if comp.get_label() == "CPU" { wanted_temp = comp.get_temperature(); } }
+    
+    wanted_temp as i32
+}
 
 fn main()
 {
@@ -65,11 +72,12 @@ fn main()
 
         let cpu_avg = get_cpu_use(&current_sys);
         let ram_prcnt = get_ram_use(&current_sys);
-        let swp_prcnt = get_swp_use(&current_sys);
+        let _swp_prcnt = get_swp_use(&current_sys);
         let ntwk_dwn = get_ntwk_dwn(&current_sys);
         let ntwk_up = get_ntwk_up(&current_sys);
+        let temp = get_temp(&current_sys);
 
-        let msg = format! ("CPU: {:.1}%\tRAM: {:.1}%\tSWP: {:.2}%\tDownload: {}Kbps\tUpload: {}Kbps", cpu_avg, ram_prcnt, swp_prcnt, ntwk_dwn, ntwk_up);
+        let msg = format! ("CPU: {:.1}%\t\tTemp: {}°C\t\tRAM: {:.1}%\t\tDownload: {}Kbps\t\tUpload: {}Kbps", cpu_avg, temp, ram_prcnt, ntwk_dwn, ntwk_up);
 
         std::process::Command::new("xsetroot").arg("-name").arg(msg).spawn().expect("`xsetroot` has failed!");
 
